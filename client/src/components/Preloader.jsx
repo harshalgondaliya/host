10c0ab8827846a24455e96781929e41ec0d6d3b9
Harshal Gondaliya <harshalgondaliya07@gmail.com>
2025-03-21 23:39:56 +0530

    final

--- client/src/components/Preloader.jsx
+++ client/src/components/Preloader.jsx
@@ -28,7 +28,7 @@ const Preloader = ({ setLoading }) => {
       // Keep preloader visible a little longer even if videos load quickly
       const minDisplayTime = setTimeout(() => {
         setLoading(false);
-      }, 4000); 
+      }, 3600); 
       
       return () => clearTimeout(minDisplayTime);
     };
